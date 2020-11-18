92c2e230b58a03fc47d74adf154a8ef22186a31b
Gabriel Martinusso <gabrielbramos@outlook.com>
2020-11-18 08:28:50 -0300

    Certs added

--- api/src/index.js
+++ api/src/index.js
@@ -3,6 +3,15 @@ const cors = require("cors");
 const app = require("express")();
 const bodyParser = require("body-parser");
 const { sequelize } = require("./models");
+const https = require("https");
+const fs = require("fs");
+
+let key = fs.readFileSync(__dirname + "/certs/private.key");
+let cert = fs.readFileSync(__dirname + "/certs/certificate.crt");
+let options = {
+  key: key,
+  cert: cert,
+};
 
 require("dotenv").config();
 
@@ -12,13 +21,17 @@ app.use(cors());
 
 app.use(_baseUrl, require("./routes"));
 
+let server =
+  process.env.HTTPS === "true" ? https.createServer(options, app) : app;
+
+
 sequelize
   .sync({ 
     // logging: false, 
     // force: true 
   })
   .then(() => {
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log("dbInit.js: Database synced");
       console.log("Online! Rodando na porta " + process.env.PORT);
     });
